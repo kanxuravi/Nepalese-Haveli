7a8b4b760b75d130ad9a9e46b81a590f06c90c9e
kanxuravi <kanxuravi9680@gmail.com>
2019-11-17 01:08:34 +0530

    working on gallery

--- app/src/main/java/com/example/nepalesehaveli/aboutus.java
+++ app/src/main/java/com/example/nepalesehaveli/aboutus.java
@@ -1,6 +1,9 @@
 package com.example.nepalesehaveli;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,12 @@ public class aboutus extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aboutus);
         setTitle("About Nepalese Haveli");
+    }
+
+    public void locate(View view) {
+        Uri gmmIntentUri = Uri.parse("https://www.google.com/maps/place/Nepalese+Haveli/@30.3534558,77.8863518,17z/data=!3m1!4b1!4m5!3m4!1s0x390f2b9a3e3dbfbf:0x255fb93100b56d83!8m2!3d30.3534512!4d77.8885405");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }
--- app/src/main/java/com/example/nepalesehaveli/gallery.java
+++ app/src/main/java/com/example/nepalesehaveli/gallery.java
@@ -1,15 +1,37 @@
 package com.example.nepalesehaveli;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class gallery extends AppCompatActivity {
+    ImageView img1 = findViewById(R.id.imageView2);
+    ImageView img2 = findViewById(R.id.imageView3);
+    ImageView img3 = findViewById(R.id.imageView4);
+    ImageView img4 = findViewById(R.id.imageView5);
+    ImageView img5 = findViewById(R.id.imageView6);
+    ImageView img6 = findViewById(R.id.imageView7);
+    ImageView img7 = findViewById(R.id.imageView8);
+    ImageView img8 = findViewById(R.id.imageView9);
+    ImageView img9 = findViewById(R.id.imageView10);
+    TextView def = findViewById(R.id.viewDef);
+    ImageView imgdef = findViewById(R.id.newImgDef);
+
     @Override
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gallery);
         setTitle("Gallery of Nepalese Haveli");
+    }
+
+
+    public void img1(View view) {
+
     }
 }
